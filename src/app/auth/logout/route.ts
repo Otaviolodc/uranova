48ab45b861b589d646b35df377b6dd10729abf71
Otaviolodc <tavinholodc10@gmail.com>
2026-06-18 19:48:35 -0300

    fix auth routes and logout

--- src/app/auth/logout/route.ts
+++ src/app/auth/logout/route.ts
@@ -1,16 +1,12 @@
-import { createClient }
-from "@/lib/supabase/server";
+import { createClient } from "@/lib/supabase/server";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-
-  const supabase =
-    await createClient();
+export async function GET(request: Request) {
+  const supabase = await createClient();
 
   await supabase.auth.signOut();
 
   return NextResponse.redirect(
-    new URL("/login", process.env.NEXT_PUBLIC_SITE_URL)
+    new URL("/login", request.url)
   );
-
 }
