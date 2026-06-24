47a8dad86b0c02a81fb75f706da8789ebecb3612
Otaviolodc <tavinholodc10@gmail.com>
2026-06-24 16:05:25 -0300

    Teste dashboard auth

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react";
-import { redirect } from "next/navigation";
 
 import Sidebar from "@/components/layout/Sidebar";
 import Topbar from "@/components/layout/Topbar";
@@ -12,32 +11,28 @@ export default async function DashboardLayout({
 }: {
   children: ReactNode;
 }) {
-
   const supabase = await createClient();
 
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (!user) {
-    redirect("/auth/login");
-  }
+  console.log(
+    "DASHBOARD LAYOUT USER:",
+    user?.email
+  );
 
   return (
     <div className="flex bg-black min-h-screen">
-
       <Sidebar />
 
       <div className="flex flex-col flex-1 min-h-screen">
-
         <Topbar />
 
         <main className="flex-1 overflow-y-auto">
           {children}
         </main>
-
       </div>
-
     </div>
   );
 }
